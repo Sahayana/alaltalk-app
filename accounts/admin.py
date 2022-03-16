11617f3f9dc6a74a8bf84897506d6295d2234d93
sahayana <saokhueyang@gmail.com>
2022-03-16 22:06:16 +0900

    UserAdmin 주석 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -20,6 +20,7 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('email',)
 
     # UserAdmin fieldsets override: 완전히 새로운 User모델을 만들었기 때문에 fieldsets override 안하면 admin페이지에서 오류
+    # User detail 페이지에서 보여줄 필드를 section화 해서 보여줌
     fieldsets = (
         (None, {'fields': ('id', 'email', 'password')}),
         ('Personal info', {'fields': ('nickname', 'bio', 'img')}),
@@ -27,7 +28,7 @@ class CustomUserAdmin(UserAdmin):
         ('Date', {'fields': ('date_joined', 'last_login')})
     )
 
-    # admin 페이지에서 사용자 추가할때 입력폼
+    # admin 페이지에서 사용자 '새로' 추가할때 더할 fieldset
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
