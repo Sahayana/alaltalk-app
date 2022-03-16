9688691f2023e454d66d620e40d3d459f55141ec
sahayana <saokhueyang@gmail.com>
2022-03-16 15:55:24 +0900

    CustomUser Admin 생성

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm, UserChangeForm, UserCreationForm
+from accounts.models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    list_display = ['email', 'nickname']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(Group)
