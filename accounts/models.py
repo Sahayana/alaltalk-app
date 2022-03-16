cf52d885e084db366b48f3d3112652d0764d6072
sahayana <saokhueyang@gmail.com>
2022-03-16 17:12:18 +0900

    유저모델 수정

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
 
-    nickname = models.CharField(max_length=60)
+    nickname = models.CharField(max_length=30, unique=True)
     bio = models.CharField(max_length=150, blank=True)
     img = models.ImageField(default="https://sahayana-nts.s3.ap-northeast-2.amazonaws.com/style_pepe.png", null=True, blank=True)
     
--- alaltalk/settings.py
+++ alaltalk/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
