88542697864724c1a57376c5a5683f43f48d1d3d
zhqmfkv <sk49229038@gmail.com>
2022-03-21 16:40:11 +0900

    work222

--- search/functions.py
+++ search/functions.py
@@ -1,10 +1,10 @@
 from typing import List
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import time
+from search.apps import SearchConfig
 
 
-def crawling_youtube(search_word: str,  content_count: int) -> List[str]:
+def crawling_youtube(search_word: str,  content_count: int) -> List[List[str]]:
     start = time.time()
     # 검색 단어 분리
     search_word = search_word.replace(' ', '+')
@@ -12,18 +12,7 @@ def crawling_youtube(search_word: str,  content_count: int) -> List[str]:
     # 클로링을 담을 배열
     crawling_data = []
 
-    # chrome webdriver 실행
-    options = webdriver.ChromeOptions()
-
-    # webdriver option 설정 - 창 안 보이기, 시크릿 모드
-    options.add_argument("headless")
-    options.add_argument('--incognito')
-
-    # driver 위치
-    driver = webdriver.Chrome(executable_path="D:/projects/alaltalk-app/search/chromedriver.exe", options=options)
-
-    # driver 쿠키 제거
-    driver.delete_all_cookies()
+    driver = SearchConfig.driver
 
     # 유튜브 링크 들어 가기 ( 검색 결과한 결과 )
     url = 'https://www.youtube.com/results?search_query=' + search_word
@@ -32,8 +21,10 @@ def crawling_youtube(search_word: str,  content_count: int) -> List[str]:
 
     # 목표 량이 10개를 넘을 때까지 반복
     while len(crawling_temp_data) < content_count:
+        print('hi')
         # 스크롤 내리기
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        driver.execute_script('window.scrollTo(0, window.scrollY + 10000);')
+
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -42,12 +33,25 @@ def crawling_youtube(search_word: str,  content_count: int) -> List[str]:
 
     # 각 검색 결과에서 URL 얻기
     for data in crawling_temp_data:
+        print('##########################################')
+        video_channels = data.select_one('#text-container a').text
+        video_channels_link = data.select_one('#text-container a').attrs['href']
+        video_views = data.select('span.ytd-video-meta-block')[0].text
         video_url = data.select('a')[0].attrs['href']
+        video_title = data.select('a')[1].attrs['title']
         youtube_base_url = 'https://www.youtube.com'
-        crawling_url = youtube_base_url + video_url
-        crawling_data.append(crawling_url)
+        if '/shorts' in video_url:
+            continue
+        else:
+            crawling_url = str(video_url).split('=')[1]
+        crawling_url = youtube_base_url + '/embed/' + crawling_url
+        crawling_data.append([crawling_url, video_url, video_title, video_views])
 
-    return crawling_data[0:content_count+1]
+    print('function time is ', time.time()-start, 'seconds')
+    return crawling_data[0:content_count]
+
+
+print(crawling_youtube('노래', 30))
 
 # 최신 동영상, 관련 비디오
 # crawling_temp_data_latest_video = soup.select('#contents > ytd-shelf-renderer')
