a325145829abced7b489b6d67807431cebfbd0c1
zhqmfkv <sk49229038@gmail.com>
2022-03-22 23:42:40 +0900

    하진님 crawling_news

--- search/service/search_service.py
+++ search/service/search_service.py
@@ -1,5 +1,4 @@
 from typing import List
-import csv
 import urllib.request
 from urllib import parse
 from bs4 import BeautifulSoup
@@ -35,6 +34,11 @@ def crawling_news(search: str) -> List[List[str]]:
 
     return answer
 
+    # [
+    #     [내용, 내용],
+    #     [내용, 내용],
+    # ]
+
     # title = soup.findall(class='news_tit')
     # test_list = soup.select('#main_pack > section > div > div.group_news > ul>li ')
     # test_list = soup.select('#sp_nws1 > div > div > div.news_info')
@@ -45,3 +49,30 @@ def crawling_news(search: str) -> List[List[str]]:
     #     print(i.attrs['title'])
     #     print(i.attrs['href'])
     #     print('\n')
+
+
+def crawling_book(search: str) -> List[List[str]]:
+    answer = []
+
+    context = ssl._create_unverified_context()
+
+    # search = input("검색어를 입력하세요:")
+    url = 'https://search.kyobobook.co.kr/web/search?vPstrKeyWord='
+    newUrl = url+parse.quote(search)
+
+    test_title = []
+
+    html = urllib.request.urlopen(newUrl, context=context).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    search_list = soup.select('#search_list tr')
+
+    for row in search_list:
+        print('#####################')
+        print(row)
+        print(row.select_one('detail'))
+
+    return answer
+
+
+crawling_book('코딩')
