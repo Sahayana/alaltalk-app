c4c8c92d26d08f636d800d764f840847f64773f6
kimpearl3599 <kimpearl3599@gmail.com>
2022-03-24 22:41:57 +0900

    add exceptions in chat app

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from chat import views
 app_name = "chat"
 
 urlpatterns = [
-    path("userlist/", views.get_user_list, name="userlist"),
+    # path("userlist/", views.get_user_list, name="userlist"),
     path("", views.show_chat_list, name="show_chat_list"),
     path("<int:id>/", views.create_chat_room, name="create_chat_room"),
     path("room/<int:room_id>/", views.create_chat_message, name="create_chat_message"),
--- search/apps.py
+++ search/apps.py
@@ -7,8 +7,8 @@ class SearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
 
     # 현재 폴더 + chromedriver 경로 붙이기
-    webdriver_path = os.getcwd() + '\search\chromedriver.exe'
-    print(webdriver_path)
+    # webdriver_path = os.getcwd() + '\search\chromedriver.exe'
+    # print(webdriver_path)
     name = "search"
     # chrome webdriver 실행
     options = webdriver.ChromeOptions()
@@ -16,7 +16,7 @@ class SearchConfig(AppConfig):
     # webdriver option 설정 - 창 안 보이기, 시크릿 모드
     options.add_argument("headless")
     options.add_argument('--incognito')
-    driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
 
     # 쿠기 삭제
     driver.delete_all_cookies()
