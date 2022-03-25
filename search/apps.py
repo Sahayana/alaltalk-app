b522a44446d2c761dbf46551a08a44604b365d8d
DESKTOP-E1D17BN\ysh <ysh0714@naver.com>
2022-03-25 13:36:05 +0900

    드라이버 경로

--- search/apps.py
+++ search/apps.py
@@ -8,7 +8,7 @@ class SearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
 
     # 현재 폴더 + chromedriver 경로 붙이기
-    # webdriver_path = os.getcwd() + '\search\chromedriver.exe'
+    webdriver_path = os.getcwd() + '\search\chromedriver.exe'
     # print(webdriver_path)
     name = "search"
     # chrome webdriver 실행
@@ -17,7 +17,7 @@ class SearchConfig(AppConfig):
     # webdriver option 설정 - 창 안 보이기, 시크릿 모드
     options.add_argument("headless")
     options.add_argument("--incognito")
-    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
+    driver = webdriver.Chrome(executable_path=webdriver_path, options=options)
 
     # 쿠기 삭제
     driver.delete_all_cookies()
