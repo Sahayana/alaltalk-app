2cdfd382f71173a8f44570ccc4016f211bb6d5d3
zhqmfkv <zhqmfkv@naver.com>
2022-04-07 00:15:46 +0900

    like 테스트 코드 작성,  youtube, book, news, shopping 좋아요 했을때, 유저없었을때, 이미 존재하는 좋아요일때

--- search/tests/apis/test_like_router.py
+++ search/tests/apis/test_like_router.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, RequestFactory
 from accounts.services.accounts_service import create_single_user
-from search.models import Youtube
+from search.models import Youtube, News, Book, Shopping
 
 
 class TestLikeCancelService(TestCase):
@@ -41,7 +41,7 @@ class TestLikeCancelService(TestCase):
         self.assertEqual(201, response.status_code)
         self.assertEqual('success', response.json()['result'])
 
-    # 유튜브 이미 등록된
+    # 유튜브 : 이미 등록된 유튜브 좋아요 테스트
     def test_already_like_youtube(self) -> None:
         # Given
         # 유저 로그인
@@ -71,6 +71,7 @@ class TestLikeCancelService(TestCase):
         self.assertEqual(201, response.status_code)
         self.assertEqual('AlreadyExist', response.json()['result'])
 
+    # 유튜브 : 유저가 없을 때 유튜브 좋아요
     def test_like_youtube_user_does_not_exist(self) -> None:
         # Given
         request = self.factory.get('api/like/youtube')
@@ -92,7 +93,7 @@ class TestLikeCancelService(TestCase):
         self.assertEqual(404, response.status_code)
         self.assertEqual('User is not Exist', response.json()['detail'])
 
-    # 뉴스 찜 테스트
+    # 뉴스 : 찜 테스트
     def test_like_news(self) -> None:
         # Given
         self.client.login(email=self.email, password=self.password)
@@ -121,6 +122,74 @@ class TestLikeCancelService(TestCase):
         # Then
         self.assertEqual(201, response.status_code)
 
+    # 뉴스 : 이미 있는 뉴스 좋아요 테스트
+    def test_like_news_already_news(self) -> None:
+        # Given
+        self.client.login(email=self.email, password=self.password)
+        request = self.factory.get('/api/like/news')
+        request.user = self.user
+        title = 'news_title'
+        date = 'news_data'
+        link = 'news_link'
+        company = 'news_company'
+        content = 'news_content'
+        thumbnail = 'news_thumbnail'
+        News.objects.create(
+            title=title,
+            user=self.user,
+            date=date,
+            link=link,
+            company=company,
+            content=content,
+            thumbnail=thumbnail
+        )
+
+        # When
+        response = self.client.post(
+            f"/api/like/news",
+            data={
+                "title": title,
+                'date': date,
+                'link': link,
+                'company': company,
+                'content': content,
+                'thumbnail': thumbnail,
+            },
+        )
+
+        # Then
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('AlreadyExist', response.json()['result'])
+
+    # 뉴스 좋아요 테스트 : 없는 유저 일 경우
+    def test_like_news_user_already_exist(self) -> None:
+        # Given
+        request = self.factory.get('/api/like/news')
+        request.user = self.user
+        title = 'news_title'
+        date = 'news_data'
+        link = 'news_link'
+        company = 'news_company'
+        content = 'news_content'
+        thumbnail = 'news_thumbnail'
+
+        # When
+        response = self.client.post(
+            f"/api/like/news",
+            data={
+                "title": title,
+                'date': date,
+                'link': link,
+                'company': company,
+                'content': content,
+                'thumbnail': thumbnail,
+            },
+        )
+
+        # Then
+        self.assertEqual(404, response.status_code)
+        self.assertEqual('User does not exist!!!', response.json()['detail'])
+
     def test_like_book(self) -> None:
         # Given
         self.client.login(email=self.email, password=self.password)
@@ -130,7 +199,7 @@ class TestLikeCancelService(TestCase):
         date = 'book_data'
         link = 'book_link'
         company = 'book_company'
-        content = 'book_content'
+        author = 'book_author'
         thumbnail = 'book_thumbnail'
         series= 'book_series'
 
@@ -142,7 +211,7 @@ class TestLikeCancelService(TestCase):
                 'price': date,
                 'link': link,
                 'company': company,
-                'author': content,
+                'author': author,
                 'thumbnail': thumbnail,
                 'series': series,
             },
@@ -151,6 +220,80 @@ class TestLikeCancelService(TestCase):
         # Then
         self.assertEqual(201, response.status_code)
 
+    # 이미 책을 좋아요 눌렀을 경우
+    def test_like_book_already_like(self) -> None:
+        # Given
+        self.client.login(email=self.email, password=self.password)
+        request = self.factory.get('/api/like/book')
+        request.user = self.user
+        title = 'book_title'
+        price = 'book_price'
+        link = 'book_link'
+        company = 'book_company'
+        thumbnail = 'book_thumbnail'
+        series = 'book_series'
+        author = 'book_author'
+
+        Book.objects.create(
+            user=self.user,
+            title=title,
+            link=link,
+            price=price,
+            company=company,
+            thumbnail=thumbnail,
+            series=series,
+            author=author
+        )
+
+        # When
+        response = self.client.post(
+            f"/api/like/book",
+            data={
+                'title': title,
+                'link': link,
+                'price': price,
+                'company': company,
+                'thumbnail': thumbnail,
+                'series': series,
+                'author': author
+            },
+        )
+
+        # Then
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('AlreadyExist', response.json()['result'])
+
+    # 책 좋아요 : 유저가 없는 경우
+    def test_like_book_user_does_not_exist(self) -> None:
+        # Given
+        request = self.factory.get('/api/like/book')
+        title = 'book_title'
+        price = 'book_price'
+        link = 'book_link'
+        company = 'book_company'
+        thumbnail = 'book_thumbnail'
+        series = 'book_series'
+        author = 'book_author'
+
+        # When
+        response = self.client.post(
+            f"/api/like/book",
+            data={
+                'title': title,
+                'link': link,
+                'price': price,
+                'company': company,
+                'thumbnail': thumbnail,
+                'series': series,
+                'author': author
+            },
+        )
+
+        # Then
+        self.assertEqual(404, response.status_code)
+        self.assertEqual('User does not exist!', response.json()['detail'])
+
+    # 쇼핑 좋아요 테스트 : 좋아요
     def test_like_shopping(self) -> None:
         # Given
         self.client.login(email=self.email, password=self.password)
@@ -174,3 +317,61 @@ class TestLikeCancelService(TestCase):
 
         # Then
         self.assertEqual(201, response.status_code)
+
+    # 쇼핑 좋아요 테스트 : 좋아요가 이미 있을 때
+    def test_like_shopping_already_like(self) -> None:
+        # Given
+        self.client.login(email=self.email, password=self.password)
+        request = self.factory.get('/api/like/shopping')
+        request.user = self.user
+        title = 'shopping_title'
+        link = 'shopping_link'
+        price = 'shopping_price'
+        thumbnail = 'shopping_thumbnail'
+
+        Shopping.objects.create(
+            user=self.user,
+            title=title,
+            link=link,
+            price=price,
+            thumbnail=thumbnail
+        )
+
+        # When
+        response = self.client.post(
+            f"/api/like/shopping",
+            data={
+                "title": title,
+                'link': link,
+                'price': price,
+                'thumbnail': thumbnail,
+            },
+        )
+
+        # Then
+        self.assertEqual(201, response.status_code)
+        self.assertEqual('AlreadyExist', response.json()['result'])
+
+    # 쇼핑 좋아요 테스트 : 유저가 없다면
+    def test_like_shopping_user_does_not_exist(self) -> None:
+        # Given
+        request = self.factory.get('/api/like/shopping')
+        title = 'shopping_title'
+        link = 'shopping_link'
+        price = 'shopping_price'
+        thumbnail = 'shopping_thumbnail'
+
+        # When
+        response = self.client.post(
+            f"/api/like/shopping",
+            data={
+                "title": title,
+                'link': link,
+                'price': price,
+                'thumbnail': thumbnail,
+            },
+        )
+
+        # Then
+        self.assertEqual(404, response.status_code)
+        self.assertEqual('User does not exist!!!', response.json()['detail'])
