6e22a387dc5aac7c44c73b6e9f6a3c1d8fb508de
sahayana <saokhueyang@gmail.com>
2023-11-22 14:28:31 +0900

    수정: asgi application 설정 - ws 요청 websocket - http 요청 asgi

--- alaltalk/asgi.py
+++ alaltalk/asgi.py
@@ -9,23 +9,21 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import SessionMiddlewareStack
 from django.core.asgi import get_asgi_application
 
-# import apps.chat.routing
+from apps.chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alaltalk.settings.dev")
+django.setup()
 
 # chat.router를 root router로 설정
-# application = ProtocolTypeRouter(
-#     {
-#         "http": get_asgi_application(),
-#         "websocket": SessionMiddlewareStack(
-#             URLRouter(apps.chat.routing.websocket_urlpatterns)
-#         ),
-#     }
-# )
-
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": SessionMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
